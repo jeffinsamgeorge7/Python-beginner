a7c7770721ebf9a18b6089c7b8f609198d5b5c80
JEFFIN SAM GEORGE <76589856+WolfCoder-Jeff@users.noreply.github.com>
2021-06-09 07:44:07 -0700

    Add files via upload

---
+++ Simpleinterest.py
@@ -0,0 +1,8 @@
+p=int(input("Enter the principal amount: "))
+
+n=int(input("Enter the number of year: "))
+
+r=int(input("Enter the rate of interest: "))
+
+si=int((p*n*r)/100)
+print("The simple interest of "+str(p)+" rupees is " +str(si))
